599e8e6451010c351b6a7def27f2b7ede7c01a4b
meenakshi-kondal <manukondal20@gmail.com>
2026-04-27 16:25:55 +0530

    push

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
-
+// const BASE_URL = "http://localhost:5000/api";
+// for production
+const BASE_URL = "https://my-diary-backend-pbx6.onrender.com/api";
 export default BASE_URL;
