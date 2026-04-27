fb3fcffe265ea928bb4599433bb083a0de96b832
meenakshi-kondal <manukondal20@gmail.com>
2026-04-27 16:31:06 +0530

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,2 @@
-// const BASE_URL = "http://localhost:5000/api";
-// for production
-const BASE_URL = "https://my-diary-backend-pbx6.onrender.com/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 export default BASE_URL;
